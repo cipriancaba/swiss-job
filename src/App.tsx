cf6580132c18a7d01f8c0db207ecc2f0e296b4bf
Ciprian Caba <caba.ciprian@gmail.com>
2018-10-25 14:09:49 +0100

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,12 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <aside>Hello</aside>
+        <main>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </main>
       </div>
     )
   }
